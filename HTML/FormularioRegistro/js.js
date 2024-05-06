af3f1dbf663bd1c233f1b35d97c0bfdb4402a7ac
“poncedavid” <Da.ponce@duocuc.cl>
2024-05-06 19:05:58 -0400

    formulario v2

---
+++ HTML/FormularioRegistro/js.js
@@ -0,0 +1,12 @@
+primeroNumero = 15;
+console.log(primeroNumero);
+
+function multiplicaNumero(numero){
+
+   console.log(numero);   
+
+   return numero*2;
+}
+
+resultado = multiplicaNumero(100);
+console.log(resultado);
